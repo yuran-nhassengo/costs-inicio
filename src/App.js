c939a810547b39aa93d9ff0a8abe69cb4ea3c9f9
yuran-nhassengo <yurannhassengo@gmail.com>
2023-01-26 11:17:35 +0200

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import Home from './componentes/pages/Home';
+import Company from './componentes/pages/Company';
+import Contacto from './componentes/pages/Contacto';
+import NewProject from './componentes/pages/NewProject';
+import NavBar from './componentes/pages/NavBar';
 
 
 function App() {
+
   return (
-    <div >
-      
-    </div>
+    <Router>
+
+        <NavBar/>
+
+          <Routes>
+
+              <Route path="/"  element={<Home/>} />
+
+              <Route path ="/company" element ={<Company />} />
+
+              <Route path="/contacto" element={<Contacto/>}/>
+
+              <Route path="/newproject" element={<NewProject />}/>
+
+          </Routes>
+
+    </Router>
+
   )
 }
 
-export default App;
+export default App
