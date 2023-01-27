f7e324831bfe6433cad4c88c186010b6ca628363
yuran-nhassengo <yurannhassengo@gmail.com>
2023-01-27 13:17:17 +0200

    terminando-home-page

--- src/componentes/pages/Home.js
+++ src/componentes/pages/Home.js
@@ -1,6 +1,22 @@
+import styles from './Home.module.css'
+import savings from '../../img/saving.svg'
+import LinkButton from '../layout/LinkButton.js'
+
+
 function Home(){
 
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos </p>
+            
+            <LinkButton to ="/newproject" text="Criar Projecto" />
+
+            <img src ={savings} alt='Costs'/>
+
+        </section>
+    )
 
 }
 
